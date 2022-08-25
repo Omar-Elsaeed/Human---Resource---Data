760f56dbdecd3bc1600ee1434427bb08e36df365
Omar ElSaied <48236730+Omar-Elsaeed@users.noreply.github.com>
2022-08-25 20:12:56 +0200

    Add files via upload
    In The Excel File -- im doing power Query Combine for all file's to make an one table & Clean prossec 
    In The Python File "JuypterNotebook" -- im Doing EDA For this data & some simple trying For Matplotlib charts
    In SQL File -- Im Trying To type 3 Querye's For EDA This Data But im really doing more practice On SQL whith time
    Finally In power Bi File -- it just a nice Dashboard  ihope it's help for Clear vision And Answer on Questions
    Thanks alot

---
+++ the queres.sql
@@ -0,0 +1,24 @@
+/* SELECT 
+FROM Hr
+WHERE Age < 30
+GROUP BY FP , Gender , TermDate , PayType
+ORDER BY EmpID DESC;*/
+
+
+/* SELECT 
+FROM Hr
+WHERE Gender = F 
+GROUP BY FP , Gender , TermDate , PayType , AgeGroup
+ORDER BY Age ;*/
+
+/* SELECT COUNT EmpID , BURegion
+FROM Hr
+GROUP BY EthnicGroup
+ORDER BY COUNT EmpID DESC; */
+
+
+
+
+
+
+
